f7db454aadabf44b335fa7ff865a28e1a92ef57b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-06-17 21:56:53 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.homewidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_code/actions/index.dart
@@ -0,0 +1,2 @@
+export 'home_screen_widget.dart' show homeScreenWidget;
+export 'initialize_home_widget.dart' show initializeHomeWidget;
